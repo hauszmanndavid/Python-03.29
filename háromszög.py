599040030e51859ade63768cbaa997b549d5c39f
hauszmanndavid <hauszmannd@gmail.com>
2023-03-31 15:24:49 +0200

    FizzBuzz

--- háromszög.py
+++ háromszög.py
@@ -1,1 +1,15 @@
 class Háromszög:
+    def Kerulet(A,B,C):
+        return round(A + B + C, 4)
+
+
+
+
+A = input("A oldal: ")
+B = input("B oldal: ")
+C = input("C oldal: ")
+
+f = open("eredmeny.txt","r")
+kerület = Kerulet(A,B,C)
+
+print("Kerület: ", kerület)
