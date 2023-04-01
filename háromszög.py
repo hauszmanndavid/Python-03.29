a30de6ae5c628b0ccaaa826a94124379136803cd
hauszmanndavid <hauszmannd@gmail.com>
2023-04-01 08:34:59 +0200

    aasd

--- háromszög.py
+++ háromszög.py
@@ -1,19 +1,15 @@
 class Háromszög:
-    def szerkeszthetőség(o):
-        if ((A+B>C) and (A+C>B) and (B+C>A)):
-            return "Szerkeszthető "+str(kerulet(A+B+C))
-        else:
-            return "Nem szerkeszthető"
-    
-    def kerulet(A,B,C):
-        return A+B+C
+    def Kerület(a,b,c):
+         return round(a + b + c, 4)
 
 
-f = open("eredmeny.txt","r")
+a = float(input("A oldal: "))
+b = float(input("B oldal: "))
+c = float(input("C oldal: "))
 
-A = input("A oldal: ")
-B = input("B oldal: ")
-C = input("C oldal: ")
 
-print(szerkeszthetőség(o))
+kerület= Kerület(a,b,c)
+
+
+print("Perimeter:", kerület)
 
